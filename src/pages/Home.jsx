5628813cc938d78f01e726a82d7371d670b2ef55
Abdul Lahad <lahad@example.com>
2025-12-04 23:08:32 +0500

    home page UI

---
+++ src/pages/Home.jsx
@@ -0,0 +1,41 @@
+import MovieCard from "../components/MovieCard";
+import { useState, useEffect } from "react";
+
+import "../css/Home.css";
+
+function Home() {
+  
+  
+
+  
+  return (
+    <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for movies..."
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => ln}
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+
+        {error && <div className="error-message">{error}</div>}
+
+      {loading ? (
+        <div className="loading">Loading...</div>
+      ) : (
+        <div className="movies-grid">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Home;
